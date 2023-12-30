07a3ae4b06df57b7574a5a8230fea331c8b907d3
Alon Albahari <ustaalon@gmail.com>
2023-12-30 02:40:45 +0000

    Fix issue #10

---
+++ Anubis.LC.ExtraDays/Patches/TerminalPatch.cs
@@ -0,0 +1,23 @@
+﻿using Anubis.LC.ExtraDays;
+using HarmonyLib;
+using LethalAPI.TerminalCommands.Models;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// Patching Terminal
+/// </summary>
+[HarmonyPatch(typeof(Terminal))]
+internal static class TerminalPatch
+{
+    [HarmonyPatch("BeginUsingTerminal")]
+    [HarmonyPostfix]
+    private static void BeginUsingTerminal(Terminal __instance)
+    {
+        if (StartOfRound.Instance.companyBuyingRate <= 0f)
+        {
+            ExtraDaysToDeadlineStaticHelper.Logger.LogInfo("Company buying rate is lower than zero, recalculating...");
+            TimeOfDay.Instance.ReCalculateBuyingRateForCompany();
+        }
+    }
+}
