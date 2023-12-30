a837cf8f13f86767ffe16b0c9b966808223d9a75
Alon Albahari <ustaalon@gmail.com>
2023-12-30 01:47:03 +0000

    Fix issue #9

---
+++ Anubis.LC.ExtraDays/Patches/TerminalPatch.cs
@@ -0,0 +1,19 @@
+﻿using Anubis.LC.ExtraDays;
+using HarmonyLib;
+using LethalAPI.TerminalCommands.Models;
+using System.Collections.Generic;
+
+
+/// <summary>
+/// Patching Terminal
+/// </summary>
+[HarmonyPatch(typeof(Terminal))]
+internal static class TerminalPatch
+{
+    [HarmonyPatch("QuitTerminal")]
+    [HarmonyPostfix]
+    private static void QuitTerminal(Terminal __instance)
+    {
+        CommandHandler.HandleCommandInput("help", __instance);
+    }
+}
