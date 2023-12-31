a427cc9fc59458c4b1deb3d46ce1738b35c80341
Alon Albahari <ustaalon@gmail.com>
2023-12-31 01:18:20 +0000

    fixing merge issues

--- Anubis.LC.ExtraDays/Extensions/TimeOfDayExtensions.cs
+++ Anubis.LC.ExtraDays/Extensions/TimeOfDayExtensions.cs
@@ -11,6 +11,8 @@ namespace Anubis.LC.ExtraDays.Extensions
 {
     public static class TimeOfDayExtensions
     {
+        private static int extraDayPrice = 0;
+
         public static void AddXDaysToDeadline(this TimeOfDay timeOfDay, float days = 1f)
         {
             timeOfDay.timeUntilDeadline += timeOfDay.totalTime * days;
@@ -33,16 +35,22 @@ namespace Anubis.LC.ExtraDays.Extensions
             return days;
         }
 
-        public static int GetExtraDaysPrice(this TimeOfDay timeOfDay)
+        public static void SetExtraDaysPrice(this TimeOfDay timeOfDay)
         {
             int profitQuota = timeOfDay.profitQuota;
             float baseIncrease = 0.15f * profitQuota;
             float minIncrease = 0.5f * profitQuota;
-            float randommizer = timeOfDay.quotaVariables.randomizerCurve.Evaluate(UnityEngine.Random.Range(0f, 1f)) * timeOfDay.quotaVariables.randomizerMultiplier + 1f;
+            float randommizer = (timeOfDay.quotaVariables.randomizerCurve.Evaluate(UnityEngine.Random.Range(0f, 1f)) * timeOfDay.quotaVariables.randomizerMultiplier + 1f);
             float priceXRandom = baseIncrease * randommizer;
 
             int price = (int)Mathf.Clamp(priceXRandom, minIncrease, 1E+09f);
-            return price;
+
+            extraDayPrice = price;
+        }
+
+        public static int GetExtraDaysPrice(this TimeOfDay timeOfDay)
+        {
+            return extraDayPrice;
         }
 
         public static void ResetDeadline(this TimeOfDay timeOfDay, bool isShipReset = false)
